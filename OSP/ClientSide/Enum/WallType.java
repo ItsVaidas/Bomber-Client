36308470087270b81f00f3193bc533fc55c9ac86
ItsVaidas <91265795+ItsVaidas@users.noreply.github.com>
2021-10-10 16:38:41 +0300

    Game screen
    Sukurtas žaidimo ekranas su sienomis (pradinis variantas).
    Ateities planai: Sukurti žaidėjus ir jų judėjimą tarp langelių.
    Gridas 10x10 (galimai toks ir liks)

---
+++ OSP/ClientSide/Enum/WallType.java
@@ -0,0 +1,11 @@
+package OSP.ClientSide.Enum;
+
+public enum WallType {
+
+	NONE(0),
+	DESTRUCTABLE(1),
+	INDISTRUCTABLE(2);
+
+	WallType(int i) {
+	}
+}
--- OSP/ClientSide/Screen/Window.java
+++ OSP/ClientSide/Screen/Window.java
@@ -1,14 +1,23 @@
 package OSP.ClientSide.Screen;
 
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
+import java.awt.GridLayout;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 
+
+import OSP.ClientSide.Enum.WallType;
 import OSP.ClientSide.SendMessage.SendMessageWithTCP;
 
 public class Window {
@@ -21,8 +30,8 @@ public class Window {
 		
 		frame = new JFrame("Bomber");
 		
-		frame.setPreferredSize(new Dimension(640, 480));
-		frame.setMinimumSize(new Dimension(640, 480));
+		frame.setPreferredSize(new Dimension(640, 640));
+		frame.setMinimumSize(new Dimension(640, 640));
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		
@@ -62,6 +71,9 @@ public class Window {
     				frame.getContentPane().removeAll();
     				addText("Waiting for more players...", 160, 200, 320);
     				repaintFrame();
+    				
+    				frame.getContentPane().removeAll();
+    				paintGame();
     			}
     			if (response[0].equals("1")) {
     				prevStatus = 1;
@@ -76,6 +88,50 @@ public class Window {
         }).start();
 	}
 	
+	String map = "2222222222200111100220111111022111111112211111111221111111122111111112201111110220011110022222222222";
+	private void paintGame() {
+		frame.setLayout(new GridLayout(10, 10));
+		
+		for (int i = 0; i < 10; i++) {
+			
+			for (int a = 0; a < 10; a++) {
+				char type = map.charAt(i * 10 + a);
+				switch (type) {
+					case '2':
+						addWall(WallType.INDISTRUCTABLE);
+						break;
+					case '1':
+						addWall(WallType.DESTRUCTABLE);
+						break;
+					case '0':
+						addWall(WallType.NONE);
+						break;
+				}
+			}
+		}
+		repaintFrame();
+	}
+	
+	private void addWall(WallType type) {
+		ImageIcon img = null;
+		switch (type) {
+			case INDISTRUCTABLE:
+				img = new ImageIcon(this.getClass().getResource("/wall.jpg"));
+				break;
+			case DESTRUCTABLE:
+				img = new ImageIcon(this.getClass().getResource("/wall2.jpg"));
+				break;
+			case NONE:
+				img = new ImageIcon(this.getClass().getResource("/none.jpg"));
+				break;
+		}
+		JLabel label = new JLabel(img);
+		label.setPreferredSize(new Dimension(64, 64));
+		label.setMaximumSize(new Dimension(64, 64));
+		frame.add(label);
+		
+	}
+
 	private void addText(String text, int x, int y, int width) {
 		JLabel b = new JLabel(text);
 		b.setBounds(x, y, width, 30);
